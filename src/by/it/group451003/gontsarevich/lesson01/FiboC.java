45579a2a343723a4eb3191bf0710fbcd3f46bde7
ALEX <alexey18032007@gmail.com>
2025-02-21 22:09:53 +0300

    Lesson1 A

--- src/by/it/group451003/gontsarevich/lesson01/FiboC.java
+++ src/by/it/group451003/gontsarevich/lesson01/FiboC.java
@@ -6,9 +6,6 @@ package by.it.group451003.gontsarevich.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -24,9 +21,41 @@ public class FiboC {
         return System.currentTimeMillis() - startTime;
     }
 
-    long fasterC(long n, int m) {
+        // Умножение двух матриц 2x2 по модулю mod
+        private static long[][] matrixMultiply(long[][] a, long[][] b, long mod) {
+            long[][] result = new long[2][2];
+            result[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % mod;
+            result[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % mod;
+            result[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % mod;
+            result[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % mod;
+            return result;
+        }
 
-        return -1L;
+        // Быстрое возведение матрицы 2x2 в степень power по модулю mod
+        private static long[][] matrixPower(long[][] mat, long power, long mod) {
+            long[][] result = {{1, 0}, {0, 1}}; // Единичная матрица
+            while (power > 0) {
+                if (power % 2 == 1) {
+                    result = matrixMultiply(result, mat, mod);
+                }
+                mat = matrixMultiply(mat, mat, mod);
+                power /= 2;
+            }
+            return result;
+        }
+
+        public static long fibonacciMod(long n, long m) {
+            if (n == 0) {
+                return 0;
+            }
+            long[][] mat = {{1, 1}, {1, 0}};
+            long[][] resultMat = matrixPower(mat, n - 1, m);
+            return resultMat[0][0];
+        }
+
+        long fasterC(long n, int m) {
+        return fibonacciMod(n, m);
+
     }
 
 
