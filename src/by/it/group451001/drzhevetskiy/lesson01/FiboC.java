db0b92e3da6420abbb35459e7c7724e482c2e109
Nik7069 <61656334+Nik7069@users.noreply.github.com>
2025-03-13 10:43:37 +0300

    Lesson1

--- src/by/it/group451001/drzhevetskiy/lesson01/FiboC.java
+++ src/by/it/group451001/drzhevetskiy/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group451001.drzhevetskiy.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -24,9 +26,26 @@ public class FiboC {
     long fasterC(long n, int m) {
         //Интуитивно найти решение не всегда просто и
         //возможно потребуется дополнительный поиск информации
-        return -1L;
+        long el1 = 0, el2 = 1;
+        ArrayList<Long> l = new ArrayList<>();// остатки от деления
+        l.add(0L);
+        l.add(1L);
+        boolean s = true;
+
+        while (s) {
+            long buf = el2;
+            el2 += el1;
+            el1 = buf;
+
+            if (el1 % m == 0 && el2 % m == 1)
+                s = false;
+            else
+                l.add(el2 % m);
+        }
+
+        n %= l.size() - 1;
+        return l.get((int) n);
     }
 
 
 }
-
