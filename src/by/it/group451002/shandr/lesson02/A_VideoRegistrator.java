cac90e96709a3c596200a0143e9e957bdf507af5
nastyashandr <nshandr368@gmail.com>
2025-03-31 09:05:33 +0300

    Lesson01 && Lesson02 && Lesson03 ABC

---
+++ src/by/it/group451002/shandr/lesson02/A_VideoRegistrator.java
@@ -0,0 +1,38 @@
+package by.it.group451002.shandr.lesson02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+Даны события events
+реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
+заданный период времени (1) было минимальным, а все события events
+были зарегистрированы.
+Алгоритм жадный. Для реализации обдумайте надежный шаг.
+*/
+
+public class A_VideoRegistrator {
+
+    public static void main(String[] args) {
+        A_VideoRegistrator instance = new A_VideoRegistrator();
+        double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
+        System.out.println(starts);                            //покажем моменты старта
+    }
+
+    //модификаторы доступа опущены для возможности тестирования
+    List<Double> calcStartTimes(double[] events, double workDuration) {
+        Arrays.sort(events);
+        List<Double> result = new ArrayList<>();
+        int i = 0;
+        while (i < events.length)
+        {
+            result.add(events[i]);
+            int k = i + 1;
+            while ((k < events.length) && (events[k] <= events[i] + workDuration))
+                k++;
+            i = k;
+        }
+        return result;
+    }
+}
