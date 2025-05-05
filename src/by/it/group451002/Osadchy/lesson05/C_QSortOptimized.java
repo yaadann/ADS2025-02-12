467fadd49c7556f3245573cd7dd31a85cc4c871a
User <kir228005@yandex.ru>
2025-05-05 17:54:16 +0300

    All

---
+++ src/by/it/group451002/Osadchy/lesson05/C_QSortOptimized.java
@@ -0,0 +1,155 @@
+package by.it.group451002.Osadchy.lesson05;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Видеорегистраторы и площадь 2.
+Условие то же что и в задаче А.
+
+        По сравнению с задачей A доработайте алгоритм так, чтобы
+        1) он оптимально использовал время и память:
+            - за стек отвечает элиминация хвостовой рекурсии
+            - за сам массив отрезков - сортировка на месте
+            - рекурсивные вызовы должны проводиться на основе 3-разбиения
+
+        2) при поиске подходящих отрезков для точки реализуйте метод бинарного поиска
+        для первого отрезка решения, а затем найдите оставшуюся часть решения
+        (т.е. отрезков, подходящих для точки, может быть много)
+
+    Sample Input:
+    2 3
+    0 5
+    7 10
+    1 6 11
+    Sample Output:
+    1 0 0
+
+*/
+
+public class C_QSortOptimized {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = C_QSortOptimized.class.getResourceAsStream("dataC.txt");
+        C_QSortOptimized instance = new C_QSortOptimized();
+        int[] result = instance.getAccessory2(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+    int[] getAccessory2(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!
+        //число отрезков отсортированного массива
+        int n = scanner.nextInt();
+        Segment[] segments = new Segment[n];
+        //число точек
+        int m = scanner.nextInt();
+        int[] points = new int[m];
+        int[] result = new int[m];
+
+        //читаем сами отрезки
+        for (int i = 0; i < n; i++) {
+            //читаем начало и конец каждого отрезка
+            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+        }
+        //читаем точки
+        for (int i = 0; i < m; i++) {
+            points[i] = scanner.nextInt();
+        }
+        //тут реализуйте логику задачи с применением быстрой сортировки
+        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
+
+        // Реализация быстрой сортировки с 3-разбиением
+        quickSort(segments, 0, segments.length - 1);
+
+        // Для каждой точки находим количество покрывающих отрезков
+        for (int i = 0; i < m; i++) {
+            result[i] = binarySearchCoveringSegments(segments, points[i]);
+        }
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+    private void quickSort(Segment[] segments, int low, int high) {
+        while (low < high) {
+            int[] pivot = partition(segments, low, high);
+            quickSort(segments, low, pivot[0] - 1);
+            low = pivot[1] + 1;
+        }
+    }
+
+    private int[] partition(Segment[] segments, int low, int high) {
+        Segment pivot = segments[low];
+        int lt = low;
+        int gt = high;
+        int i = low + 1;
+
+        while (i <= gt) {
+            int cmp = segments[i].compareTo(pivot);
+            if (cmp < 0) {
+                swap(segments, lt++, i++);
+            } else if (cmp > 0) {
+                swap(segments, i, gt--);
+            } else {
+                i++;
+            }
+        }
+        return new int[]{lt, gt};
+    }
+
+    private void swap(Segment[] segments, int i, int j) {
+        Segment temp = segments[i];
+        segments[i] = segments[j];
+        segments[j] = temp;
+    }
+
+    private int binarySearchCoveringSegments(Segment[] segments, int point) {
+        int left = 0;
+        int right = segments.length - 1;
+        int first = -1;
+
+        // Бинарный поиск первого отрезка, который может покрывать точку
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (segments[mid].start <= point) {
+                first = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // Подсчет всех подходящих отрезков
+        int count = 0;
+        if (first != -1) {
+            for (int i = first; i >= 0 && segments[i].start <= point; i--) {
+                if (point <= segments[i].stop) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    //отрезок
+    private class Segment implements Comparable {
+        int start;
+        int stop;
+
+        Segment(int start, int stop) {
+            this.start = start;
+            this.stop = stop;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Segment other = (Segment)o;
+            return Integer.compare(this.start, other.start);
+        }
+    }
+}
