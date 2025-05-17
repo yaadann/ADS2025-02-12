74e5269aa8ef6baa6ee18967aed46ebc709539c9
kavtsevich valeria <kavtsevichlera@gmail.com>
2025-05-17 11:25:28 +0300

    lesson5 A,B, C; lesson6 A,B, C; lesson7 A,B, C

---
+++ src/by/it/group410902/kavtsevich/lesson07/B_EditDist.java
@@ -0,0 +1,87 @@
+package by.it.group410902.kavtsevich.lesson07;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Итерационно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class B_EditDist {
+
+
+    int getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int m = one.length();
+        int n = two.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i][0] = i; // Если вторая строка пустая, удаляем все символы из первой строки
+        }
+        for (int j = 0; j <= n; j++)
+        {
+            dp[0][j] = j; // Если первая строка пустая, добавляем все символы второй строки
+        }
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                // Определяем стоимость замены (0 — если символы совпадают, 1 — если отличаются)
+                int replaceCost = (one.charAt(i - 1) == two.charAt(j - 1)) ? 0 : 1;
+                // Выбираем минимальное количество операций:
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, // удаление
+                                dp[i][j - 1] + 1), // вставка
+                        dp[i - 1][j - 1] + replaceCost); // замена
+            }
+        }
+
+
+
+        int result =  dp[m][n];
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = B_EditDist.class.getResourceAsStream("dataABC.txt");
+        B_EditDist instance = new B_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+    }
+
+}
