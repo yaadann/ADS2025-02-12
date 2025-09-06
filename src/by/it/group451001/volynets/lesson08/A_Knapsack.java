73ae2b93526a5de3d25e11f1655277b2ccc44c82
VladVolynets <vladislavvolynets007@gmail.com>
2025-09-06 16:56:49 +0300

    Lesson 8;9

--- src/by/it/group451001/volynets/lesson08/A_Knapsack.java
+++ src/by/it/group451001/volynets/lesson08/A_Knapsack.java
@@ -35,7 +35,7 @@ Sample Output 2:
 public class A_Knapsack {
 
     int getMaxWeight(InputStream stream ) {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!!
         Scanner scanner = new Scanner(stream);
         int w=scanner.nextInt();
         int n=scanner.nextInt();
--- src/by/it/group451001/volynets/lesson08/B_Knapsack.java
+++ src/by/it/group451001/volynets/lesson08/B_Knapsack.java
@@ -28,7 +28,7 @@ Sample Output:
 public class B_Knapsack {
 
     int getMaxWeight(InputStream stream ) {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         Scanner scanner = new Scanner(stream);
         int w=scanner.nextInt();
         int n=scanner.nextInt();
