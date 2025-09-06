66b40ce090ad761c88e6000701390d9bd1fec01d
VladVolynets <vladislavvolynets007@gmail.com>
2025-09-06 16:51:32 +0300

    Lesson 8;9

--- src/by/it/group451001/volynets/lesson09/ListC.java
+++ src/by/it/group451001/volynets/lesson09/ListC.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class ListC<E> implements List<E> {
+public class ListC<E> implements List<E> { 
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
 
