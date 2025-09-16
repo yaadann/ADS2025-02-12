ced7a6f18f1e5b9b7ca01f85337b0115f1fa9cd1
Heorhi Stseshyts <steshitz.jora2017@gmail.com>
2025-09-16 10:08:40 +0300

    Complete Lesson 8, 9

---
+++ src/by/it/group451001/steshits/lesson08/C_Stairs.java
@@ -0,0 +1,67 @@
+package by.it.group451001.steshits.lesson08;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Даны число 1<=n<=100 ступенек лестницы и
+целые числа −10000<=a[1],…,a[n]<=10000, которыми помечены ступеньки.
+Найдите максимальную сумму, которую можно получить, идя по лестнице
+снизу вверх (от нулевой до n-й ступеньки), каждый раз поднимаясь на
+одну или на две ступеньки.
+
+Sample Input 1:
+2
+1 2
+Sample Output 1:
+3
+
+Sample Input 2:
+2
+2 -1
+Sample Output 2:
+1
+
+Sample Input 3:
+3
+-1 2 1
+Sample Output 3:
+3
+
+*/
+
+public class C_Stairs {
+
+    int getMaxSum(InputStream stream ) {
+        Scanner scanner = new Scanner(stream);
+        int n=scanner.nextInt();
+        int stairs[]=new int[n];
+        for (int i = 0; i < n; i++) {
+            stairs[i]=scanner.nextInt();
+        }
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int result = 0;
+        if (n == 0) {
+            return 0;
+        }
+        int[] a = new int[n + 1];
+        a[0] = 0;
+        a[1] = stairs[0];
+        for (int i = 2; i <= n; i++) {
+            a[i] = Math.max(a[i - 1], a[i - 2]) + stairs[i - 1];
+        }
+        result = a[n];
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = C_Stairs.class.getResourceAsStream("dataC.txt");
+        C_Stairs instance = new C_Stairs();
+        int res=instance.getMaxSum(stream);
+        System.out.println(res);
+    }
+
+}
