87ffcb51548bfe62afb2ede7151652d6a5bdf9f4
wiseris <nargiz.jalilova.student@gmail.com>
2025-09-18 13:42:06 +0300

    lesson_9,A,B,C

--- src/by/it/group410902/jalilova/lesson09/ListA.java
+++ src/by/it/group410902/jalilova/lesson09/ListA.java
@@ -9,15 +9,16 @@ public class ListA<E> implements List<E> {
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
     private Object[] elements = new Object[10];
-    private int size = 0;
+    //массив для хранения элементов
+    private int size = 0;//текущее кол-во элементов
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     //////               Обязательные к реализации методы             ///////
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     @Override
-    public String toString() {
-        if (size == 0) {
+    public String toString() {//прохожу по всем элементам и собираю их в строку [,,]
+        if (size == 0) {//если список пуст
             return "[]";
         }
 
@@ -29,13 +30,13 @@ public class ListA<E> implements List<E> {
             }
         }
         sb.append("]");
-        return sb.toString();
+        return sb.toString();//возвращаем строку
 
     }
 
     @Override
     public boolean add(E e) {
-        if (size == elements.length) {
+        if (size == elements.length) {//если массив заполнен, создаем новый
             Object[] newElements = new Object[elements.length * 2];
             for (int i = 0; i < size; i++) {
                 newElements[i] = elements[i];
@@ -47,16 +48,16 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public E remove(int index) {
+    public E remove(int index) {//удаляем элемент по индексу, все последующие сдвигаем влево
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
 
         @SuppressWarnings("unchecked")
-        E removedElement = (E) elements[index];
+        E removedElement = (E) elements[index];// сохраняем удаляемый элемент
 
         for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
+            elements[i] = elements[i + 1]; // сдвигаем элементы влево на 1 позицию
         }
 
         elements[--size] = null;
@@ -66,7 +67,7 @@ public class ListA<E> implements List<E> {
     @Override
     public int size() {
         return size;
-    }
+    } //возвращение текущего кол-ва элементов
 
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
@@ -75,7 +76,7 @@ public class ListA<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
 
     @Override
-    public void add(int index, E element) {
+    public void add(int index, E element) {//вставляем элемент на конкретную позицию
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
@@ -88,7 +89,7 @@ public class ListA<E> implements List<E> {
             elements = newElements;
         }
 
-        for (int i = size; i > index; i--) {
+        for (int i = size; i > index; i--) {//сдвигаем элементы вправо
             elements[i] = elements[i - 1];
         }
 
@@ -98,7 +99,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean remove(Object o) {
+    public boolean remove(Object o) {//ищет первый элемент, который равен переданному объекту, удаляет его по индексу
         for (int i = 0; i < size; i++) {
             if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
                 remove(i);
@@ -109,7 +110,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public E set(int index, E element) {
+    public E set(int index, E element) {//заменяю элемент на указанной позиции и возвращаю старое значение
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
@@ -122,13 +123,13 @@ public class ListA<E> implements List<E> {
 
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty() { //проверяю равен ли размер нулю
         return size == 0;
     }
 
 
     @Override
-    public void clear() {
+    public void clear() {//обнуляю все элементы в массиве, сбрасываю размер к нулю
         for (int i = 0; i < size; i++) {
             elements[i] = null;
         }
@@ -137,7 +138,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public int indexOf(Object o) {
+    public int indexOf(Object o) {//иду с начала списка, ищу первый подходящий элемент
         for (int i = 0; i < size; i++) {
             if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
                 return i;
@@ -147,7 +148,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public E get(int index) {
+    public E get(int index) {//возвращаю элемент по указанному индексу
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
@@ -158,12 +159,12 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(Object o) { //использую indexOf для проверки наличия элемента
         return indexOf(o) != -1;
     }
 
     @Override
-    public int lastIndexOf(Object o) {
+    public int lastIndexOf(Object o) { //иду с конца списка и ищу последний подходящий элемент
         for (int i = size - 1; i >= 0; i--) {
             if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
                 return i;
@@ -174,7 +175,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
+    public boolean containsAll(Collection<?> c) { //для каждого элемента коллекции проверяю, есть ли он в списке
         for (Object element : c) {
             if (!contains(element)) {
                 return false;
@@ -184,7 +185,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean addAll(Collection<? extends E> c) {
+    public boolean addAll(Collection<? extends E> c) {//поочерёдно добавляю все элементы из коллекции в конец списка
         if (c.isEmpty()) {
             return false;
         }
@@ -196,7 +197,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
+    public boolean addAll(int index, Collection<? extends E> c) {//вставляю все элементы из коллекции, начиная с позиции
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
@@ -213,7 +214,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean removeAll(Collection<?> c) {
+    public boolean removeAll(Collection<?> c) {//удаляю из списка все элементы, которые есть в коллекции
         boolean modified = false;
         for (int i = 0; i < size; i++) {
             if (c.contains(elements[i])) {
@@ -226,7 +227,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public boolean retainAll(Collection<?> c) {
+    public boolean retainAll(Collection<?> c) {//оставляю в списке только элементы, которые есть в коллекции
         boolean modified = false;
         for (int i = 0; i < size; i++) {
             if (!c.contains(elements[i])) {
@@ -240,7 +241,7 @@ public class ListA<E> implements List<E> {
 
 
     @Override
-    public List<E> subList(int fromIndex, int toIndex) {
+    public List<E> subList(int fromIndex, int toIndex) { //создаю новый список и копирую в него элементы из диапазона
         if (fromIndex < 0  || toIndex > size || fromIndex > toIndex) {
             throw new IndexOutOfBoundsException();
         }
@@ -255,7 +256,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public ListIterator<E> listIterator(int index) {
+    public ListIterator<E> listIterator(int index) {//возвращает итератор, который обходит с позиции index
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
@@ -263,12 +264,12 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public ListIterator<E> listIterator() {
+    public ListIterator<E> listIterator() {//возвращает итератор, который начинает обход с начала списка
         return new ListItr(0);
     }
 
     @Override
-    public <T> T[] toArray(T[] a) {
+    public <T> T[] toArray(T[] a) {//если массив достаточно большой, копирую в него элементы, нет - создаю новый
         if (a.length < size) {
             T[] newArray = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
             for (int i = 0; i < size; i++) {
@@ -289,7 +290,7 @@ public class ListA<E> implements List<E> {
     }
 
     @Override
-    public Object[] toArray() {
+    public Object[] toArray() { //создаю новый массив и копирую в него все элементы списка
         Object[] array = new Object[size];
         for (int i = 0; i < size; i++) {
             array[i] = elements[i];
@@ -304,7 +305,7 @@ public class ListA<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     @Override
-    public Iterator<E> iterator() {
+    public Iterator<E> iterator() {//возвращаю объект, который позволяет последовательно проходить по списку
         return new Itr();
     }
     private class Itr implements Iterator<E> {
