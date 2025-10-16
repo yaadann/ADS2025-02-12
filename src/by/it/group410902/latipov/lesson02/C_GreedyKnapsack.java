99b3bb8c7320ff55f2ac30f4ace4646c36bb8377
latipowumid114-code <latipowumid114@gmail.com>
2025-10-16 14:28:24 +0300

    lesson 2 C done

--- src/by/it/group410902/latipov/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group410902/latipov/lesson02/C_GreedyKnapsack.java
@@ -1,20 +1,8 @@
 package by.it.group410902.latipov.lesson02;
-/*
-Даны
-1) объем рюкзака 4
-2) число возможных предметов 60
-3) сам набор предметов
-    100 50
-    120 30
-    100 50
-Все это указано в файле (by/it/a_khmelev/lesson02/greedyKnapsack.txt)
-
-Необходимо собрать наиболее дорогой вариант рюкзака для этого объема
-Предметы можно резать на кусочки (т.е. алгоритм будет жадным)
- */
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C_GreedyKnapsack {
@@ -40,16 +28,29 @@ public class C_GreedyKnapsack {
         }
         System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n", n, W);
 
-        //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
-        //вещи можно резать на кусочки (непрерывный рюкзак)
+        // Реализация жадного алгоритма для непрерывного рюкзака
         double result = 0;
-        //тут реализуйте алгоритм сбора рюкзака
-        //будет особенно хорошо, если с собственной сортировкой
-        //кроме того, можете описать свой компаратор в классе Item
 
-        //ваше решение.
+        // Сортируем предметы по убыванию стоимости за единицу веса
+        Arrays.sort(items);
 
+        int remainingWeight = W; // оставшаяся вместимость рюкзака
+
+        // Проходим по отсортированным предметам и добавляем в рюкзак
+        for (Item item : items) {
+            if (remainingWeight <= 0) break; // если рюкзак заполнен
+
+            if (item.weight <= remainingWeight) {
+                // Если предмет полностью помещается - берем целиком
+                result += item.cost;
+                remainingWeight -= item.weight;
+            } else {
+                // Если не помещается - берем часть предмета
+                double fraction = (double) remainingWeight / item.weight;
+                result += item.cost * fraction;
+                remainingWeight = 0; // рюкзак заполнен
+            }
+        }
 
         System.out.printf("Удалось собрать рюкзак на сумму %f\n", result);
         return result;
@@ -64,20 +65,25 @@ public class C_GreedyKnapsack {
             this.weight = weight;
         }
 
+        // Метод для вычисления стоимости за единицу веса
+        public double getCostPerUnit() {
+            return (double) cost / weight;
+        }
+
         @Override
         public String toString() {
             return "Item{" +
-                   "cost=" + cost +
-                   ", weight=" + weight +
-                   '}';
+                    "cost=" + cost +
+                    ", weight=" + weight +
+                    ", costPerUnit=" + String.format("%.2f", getCostPerUnit()) +
+                    '}';
         }
 
         @Override
         public int compareTo(Item o) {
-            //тут может быть ваш компаратор
-
-
-            return 0;
+            // Сортируем по убыванию стоимости за единицу веса
+            // Чтобы сначала брать самые "выгодные" предметы
+            return Double.compare(o.getCostPerUnit(), this.getCostPerUnit());
         }
     }
 }
